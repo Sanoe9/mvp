90f006915881438b62a6d0efdbd064e454eb1f27
Salma Noe <familypc@Salmas-Macbook-Air.local>
2021-10-15 23:07:31 -0700

    added icons to activities lists

--- client/src/components/diaper/DiapersListItem.jsx
+++ client/src/components/diaper/DiapersListItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GiArmoredPants } from 'react-icons/gi';
 
 function DiapersListItem(props) {
 
@@ -8,7 +9,8 @@ function DiapersListItem(props) {
   }
 
   return (
-    <li>
+    <li style={{listStyleType: "none"}}>
+      <GiArmoredPants />
       At {props.time}, baby had {props.note} diapers changed.
       <button onClick={handleClick}>Delete</button>
     </li>
--- client/src/components/nap/NapsList.jsx
+++ client/src/components/nap/NapsList.jsx
@@ -4,8 +4,8 @@ import NapsListItem from './NapsListItem.jsx';
 
 const NapsList = (props) => (
   <div>
-    <h5>Here is your naps' log list:</h5>
-    <ul>
+    {/* <h5>Here is your naps' log list:</h5> */}
+    <ul >
       {props.naps.map((nap, key) => ( <NapsListItem key={key} note={nap.note} time={nap.time} onDelete={props.onDelete} /> ))}
     </ul>
   </div>
